6b603f68efcd75d50a6b4bf1e8084ba2491ee2a4
Lee <leep@londonhydro.com>
2017-08-15 13:35:51 -0400

    fixed fading animation, added icons, about section, etc

--- js/index.js
+++ js/index.js
@@ -1,51 +1,58 @@
 $(document).ready(function() {
-  $('.about, .work, .projects, .contact, .press').css({'visibility':'hidden'});
+  $('#about, #work, #projects, .press, #line-separator-dark').css({'visibility':'hidden'});
 
   $(window).scroll(function() {
     var height = $(window).scrollTop();
+    var about = $('#about').position();
+    var work = $('#work').position();
+    var projects = $('#projects').position();
+    var contact = $('#contact').position();
+    var line1 = $('#line-1').position();
+    var line2 = $('#line-2').position();
 
-    if (height > 190) {
-      $('.about').css({"visibility": "visible"}).addClass('animated fadeIn');
-      $('.press').css({"visibility": "visible"}).addClass('animated fadeIn');
+    var interval = 500;
+    if (height > about.top -100) {
+      $('#about').css({opacity: 0, visibility: "visible"}).animate({opacity: 1}, interval);
     }
-
-    if (height > 350) {
-      $('.work').css({"visibility": "visible"}).addClass('animated fadeIn');
+    if (height > work.top -100) {
+      $('#work').css({opacity: 0, visibility: "visible"}).animate({opacity: 1}, interval);
     }
-    if (height > 1000) {
-      $('.projects').css({"visibility": "visible"}).addClass('animated fadeIn');
+    if (height > projects.top -100) {
+      $('#projects').css({opacity: 0, visibility: "visible"}).animate({opacity: 1}, interval);
     }
-
-    if (height > 2300) {
-      $('.contact').css({"visibility": "visible"}).addClass('animated fadeIn');
+    if (height > line1.top -100) {
+      $('#line-1').css({opacity: 0, visibility: "visible"}).animate({opacity: 1}, interval);
+    }
+    if (height > line2.top -100) {
+      $('#line-2').css({opacity: 0, visibility: "visible"}).animate({opacity: 1}, interval);
     }
 
   });
 
   /* Main button clicks */
-  $('body').on('click', '#about', function() {
+  $('body').on('click', '#nav-about', function() {
     $('html, body').animate({
-          scrollTop: $(".about").offset().top
+          scrollTop: $("#about").offset().top
     }, 500);
     return false;
   });
 
-  $('body').on('click', '#contact', function() {
+  $('body').on('click', '#nav-contact', function() {
     $('html, body').animate({
-        scrollTop: $(".contact").offset().top
+        scrollTop: $("#contact").offset().top
     }, 500);
     return false;
   });
-  $('body').on('click', '#work', function() {
+  $('body').on('click', '#nav-work', function() {
     $('html, body').animate({
-        scrollTop: $(".work").offset().top
+        scrollTop: $("#work").offset().top
     }, 500);
     return false;
   });
 
-  $('body').on('click', '#projects', function() {
+  $('body').on('click', '#nav-projects', function() {
     $('html, body').animate({
-        scrollTop: $(".projects").offset().top
+        scrollTop: $("#projects").offset().top
     }, 500);
     return false;
   });
